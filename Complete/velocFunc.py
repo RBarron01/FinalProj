93b717fbf539c240ddac7a91bdd6ae24f9477361
RBarron01 <38085810+RBarron01@users.noreply.github.com>
2018-12-17 17:12:02 -0600

    Update velocFunc.py

--- Complete/velocFunc.py
+++ Complete/velocFunc.py
@@ -3,17 +3,18 @@
 """
 Created on Mon Dec 17 11:12:16 2018
 
-@author: rachelbarron
+@author: Jodie McLennan and Rachel Barron
 """
 
 import numpy as np
 from scipy.optimize import bisect
 
 def velocSol(entEnBeam,resMEn):
-    lght = 2.99e8
-    startGuess = 1000
-    endGuess = lght/10
-    resMEn = np.abs(resMEn)
+    # to find velocity of a particle
+    lght = 2.99e8 # m/s
+    startGuess = 1000 # m/s for bisect
+    endGuess = lght/10 # m/s for bisect
+    resMEn = np.abs(resMEn) # makes sure mass is positive
     def func(veloc):    
         return np.sqrt(entEnBeam/(((1/np.sqrt(1-veloc**2/lght**2))-1)*resMEn)) - veloc
     return bisect(func,startGuess,endGuess,maxiter = 10000000) 
