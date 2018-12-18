9bcefe852c2133b502e765775790562a3d7a7b65
RBarron01 <38085810+RBarron01@users.noreply.github.com>
2018-12-17 19:38:19 -0600

    Update uncharPartPath.py

--- Complete2/uncharPartPath.py
+++ Complete2/uncharPartPath.py
@@ -3,14 +3,15 @@
 """
 Created on Mon Dec 17 15:35:04 2018
 
-@author: rachelbarron
+@author: Jodie McLennan and Rachel Barron
 """
+# tracks the path of the uncharged particles
 import numpy as np
 import random
 
-lght = 2.99e8
+lght = 2.99e8 #m/s
 
-def path0(veloc,incRad): # tracks the path of an uncharged particle
+def path0(veloc,incRad): 
     # veloc is total velocity magnitude   
     loren = 1/np.sqrt(1-veloc**2/lght**2)
     time = np.arange(0,.0000000005,10**-10)
